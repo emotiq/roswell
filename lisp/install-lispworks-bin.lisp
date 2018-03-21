7fff4881b4d95ea9188c8d1339a97ae6d5a93e6f
Mark <evenson.not.org@gmail.com>
2018-03-21 15:45:48 +0100

    Installing under MacOS with mocks to environment
    Now, move it to a local Linux testing environment to get the licensing
    working.

--- lisp/install-lispworks-bin.lisp
+++ lisp/install-lispworks-bin.lisp
@@ -9,55 +9,85 @@
   (uiop:getenv "LISPWORKS_TAR_KEY"))
 (defparameter *lispworks-serial*
   (uiop:getenv "LISPWORKS_SERIAL"))
-(defparameter *lispworks-licenses*
+(defparameter *lispworks-license*
   (uiop:getenv "LISPWORKS_LICENSE"))
 
 (defun lispworks-bin-get-version ()
-  '("1970-01-01" "0" "0"))
+  `("current"))
+
+(defun ros/archives/ ()
+  (merge-pathnames "archives/ "(homedir)))
+
+(defun ros/impls/ ()
+  (merge-pathnames
+   (format nil "impls/")
+   (homedir)))
+
+(defun uri-pathname-name-and-type (uri)
+  (let ((pos (position #\/ uri :from-end t)))
+    (let ((possible-name-and-type (subseq uri (1+ pos))))
+      (if possible-name-and-type
+          possible-name-and-type
+          uri))))
 
 (defun lispworks-bin-impl ()
   (merge-pathnames
-   (format nil "impls/~A/~A/lispworks-bin/" (uname-m) (uname)) (homedir)))
+   (format nil "~A/~A/lispworks-bin/" (uname-m) (uname))
+   (ros/impls/)))
 
 (defun lispworks-bin-download (argv)
-  `(,(merge-pathnames (format nil "archives/~a" "foo.tar.gz") (homedir))
-     *lispworks-tar-uri*)
+  (set-opt "as"
+           (getf argv :version))
+  (set-opt "download.uri"
+           *lispworks-tar-uri*)
+  (set-opt "download.archive"
+           (merge-pathnames (uri-pathname-name-and-type *lispworks-tar-uri*)
+                            (ros/archives/)))
+  `((,(opt "download.archive") ,(opt "download.uri"))))
 
 (defun lispworks-bin-install (argv)
-  (let ((uri (opt "uri"))
-        (impl-path (lispworks-bin-impl)))
-    (ensure-directories-exist impl-path)
-    (lispworks-bin-download argv)
-    ;;; download the URI
+  (let* ((root (lispworks-bin-impl))
+         (dest (merge-pathnames (format nil "~a/" (opt "as")) root)))
+    (format t "~%Extracting archive:~A~%" (opt "download.archive"))
+        ;;; TODO decrypt if necessary
+    (expand
+     (opt "download.archive")
+     (ensure-directories-exist (lispworks-bin-impl)))
+    (and (probe-file dest)
+         (uiop/filesystem:delete-directory-tree
+          dest :validate t))
+    (ql-impl-util:rename-directory
+     (merge-pathnames "lispworks/" root)
+     dest))
+  (cons t argv))
 
-    ;;; decrypt the URI
+(defun lispworks-bin-license (argv)
+  ;;; archive should have already been expanded (HOWTO check?)
+  ;;; TODO license binary
+  (uiop:run-program
+   (format nil "~a --lwlicenseserial ~a --lwlicensekey ~a"
+           "lwpro"
+           *lispworks-serial*
+           *lispworks-license*))
+  (cons t argv))
 
-    ;;; stage the URI to local filesystem
-
-    ;;; license the binary
-    (cons t argv)))
+(defun lispworks-bin-patch (argv)
+  ;;;   code needs to be private
+  (error "Unimplemented patch after licensing."))
 
 (defun lispworks-bin-help (argv)
   "Help")
-
 
 (defun lispworks-bin (type)
   (case type
     (:help '(lispworks-bin-help))
     (:list 'lispworks-bin-version)
-    (:install  `(,(decide-version 'lispworks-bin-get-version)
-                  lispworks-bin-install))))
+    (:install  `(,(decide-version 'lispworks-bin-get-version) ;; unused??
+                  ,(decide-download 'lispworks-bin-download)
+                  lispworks-bin-install
+                  lispworks-bin-license
+                  setup))))
 
 
   
-
-
-    #|
-    (:install `(,(decide-version 'abcl-bin-get-version)
-                abcl-bin-argv-parse
-                ,(decide-download 'abcl-bin-download)
-                abcl-bin-expand
-                abcl-bin-script
-                setup))
-|#
 
